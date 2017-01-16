4f9a84a126105a264a458facb933821147ded723
Theepan Karthigesan <theepan@theepans-imac.local>
2017-01-16 09:24:44 +0100

    JSON working

--- eFuel/ViewController.swift
+++ eFuel/ViewController.swift
@@ -12,6 +12,48 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+     
+        
+        
+let url = URL(string: "https://nobil.no/api/server/search.php?apikey=cd4940380eaee58fe2f84ef3d66dd572&apiversion=3&action=search&type=rectangle&northeast=(60.27513309999999,11.179013199999986)&southwest=(60.2514115,11.202181999999993)")
+        
+        
+        
+        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard error == nil else {
+                print(error ?? "feil")
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
+                return
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        }
+        
+        task.resume()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         print("Test")
         // Do any additional setup after loading the view, typically from a nib.
     }
