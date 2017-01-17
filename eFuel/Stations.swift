529348cd2d8f13a022cb98cb851846aa87f59947
Theepan Karthigesan <theepan@theepans-imac.local>
2017-01-17 21:43:51 +0100

    With notes

--- eFuel/Stations.swift
+++ eFuel/Stations.swift
@@ -6,7 +6,30 @@
 //  Copyright © 2017 Karthig.com. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Alamofire
 
 
- 
+class Stations{
+
+
+    func downloadStations(completed: DownloadComplete){
+        let currentStationsURL = URL(string : CURRENT_URL)!
+
+        
+        Alamofire.request(currentStationsURL, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+            .responseJSON { response in
+                
+                print(currentStationsURL)
+                //print(response.request as Any)  // original URL request
+                //print(response.response as Any) // URL response
+                //print(response.result.value as Any)   // result of response serialization
+        }
+        completed()
+        
+        
+        
+    
+    }
+
+}
