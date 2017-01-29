ad9b2867b16d4b682ebc4e9c860de2a15e3c4cf4
Theepan Karthigesan <tkarthig@gmail.com>
2017-01-30 00:54:05 +0100

    Last commit 29.01.2017

--- eFuel/Constants.swift
+++ eFuel/Constants.swift
@@ -14,8 +14,32 @@ import Foundation
     let API_VERSION="3"
     let ACTION="search"
     let TYPE="rectangle"
+
     let NORTHEST="(62.27513309999999,13.179013199999986)"
-    let SOUTHWEST="(60.2514115,11.202181999999993)"
+   let SOUTHWEST="(60.2514115,11.202181999999993)"
+
+
+//let NORTHEST_LANG : Double = (Location.sharedInstance.longitude / abs(cos((Location.sharedInstance.latitude * 180.0 / M_PI))) * 111)
+
+
+/*
+A point (LAT, LNG)
+A distance or radius DIST
+1° of latitude ~= 69 miles ~= 111 kms
+1° of longitude ~= cos(latitude)*69 ~= cos(latitude)*111
+The SW point is:
+
+lng_sw = LNG - (DIST / abs(cos(radians(LAT))) * 111)
+lat_sw = LAT - (DIST / 111)
+The NE point is:
+
+lng_ne = LNG + (DIST / abs(cos(radians(LAT))) * 111)
+lat_ne = LAT + (DIST / 111)
+
+
+*/
+
+
 
 
     //let NORTHEST="(37.785834,13.179013199999986)"
@@ -25,6 +49,8 @@ import Foundation
     typealias DownloadComplete = ()->()
 
 
-    let CURRENT_URL = "\(BASE_URL)apikey=\(API_KEY)&apiversion=\(API_VERSION)&action=\(ACTION)&type=\(TYPE)&northeast=\(NORTHEST)&southwest=\(SOUTHWEST)"
+   let CURRENT_URL = "\(BASE_URL)apikey=\(API_KEY)&apiversion=\(API_VERSION)&action=\(ACTION)&type=\(TYPE)&northeast=\(NORTHEST)&southwest=\(SOUTHWEST)"
+ 
+ 
 
 
